be68f21a09ec0caf9aecfa536c83ea7130080eb1
Damir D <damir180501@mail.ru>
2021-02-22 23:31:33 +0300

    Initial commit

---
+++ Assets/Scripts/DestroyObstacle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObstacle : MonoBehaviour
+{
+    public float lastZ = -100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z < lastZ)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/GenerateObstacles.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+public class GenerateObstacles : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    public GameObject obstaclesSpawnPoint;
+
+    private float time = 0;
+    private float timeMax = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > timeMax)
+        {
+            time = 0;
+            spawnObstacle();
+        }
+    }
+
+    private void spawnObstacle()
+    {
+        float rand = Random.Range(-25f, 25f);
+        Vector3 pos = new Vector3(
+            obstaclesSpawnPoint.transform.position.x,
+            obstaclesSpawnPoint.transform.position.y + rand,
+            obstaclesSpawnPoint.transform.position.z);
+        Instantiate(
+            obstaclePrefab,
+            pos,
+            obstaclesSpawnPoint.transform.rotation,
+            transform);
+    }
+}
